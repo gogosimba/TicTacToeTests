aa960e9cc456e9dad92bb9f53979b382027e6fd6
gogosimba <111742024+gogosimba@users.noreply.github.com>
2023-02-05 23:06:58 +0100

    Initial commit

---
+++ Form1Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace TicTacToe.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        Form1 testForm = new Form1();
+
+        [TestMethod()]
+        public void checkForWinner_horizontalTest()
+        {
+            //Checks horizontal win
+            
+            testForm.A1.Text = "O";
+            testForm.A2.Text = "O";
+            testForm.A3.Text = "O";
+
+            testForm.A1.Enabled = false;
+            testForm.checkForWinner();
+
+            Assert.IsTrue(testForm.winner);
+
+        }
+
+        [TestMethod()]
+        public void checkForWinner_verticalTest()
+        {
+            //Checks horizontal win
+            
+            testForm.A1.Text = "O";
+            testForm.B1.Text = "O";
+            testForm.C1.Text = "O";
+
+            testForm.A1.Enabled = false;
+            testForm.checkForWinner();
+
+            Assert.IsTrue(testForm.winner);
+
+        }
+    }
+}
